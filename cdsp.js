66cdf589854809b41fa6b96660fc6ac18629a784
Spencer Thode <spencerthode@gmail.com>
2018-02-05 14:13:25 -0800

    created index&sales.html & JS with objects

---
+++ cdsp.js
@@ -0,0 +1,32 @@
+const dpxAirport = {
+    min: '23',
+    max: '65',
+    avg: '6.3'
+};
+
+const pSquare = {
+    min:'3',
+    max: '24',
+    avg: '1.2'
+};
+
+const powells = {
+min: '11',
+max: '38',
+avg: '1.7'
+};
+
+const stJohns = {
+    min: '20',
+    max: '38',
+    avg: '2.3'
+};
+
+const waterfront = {
+    min: '2',
+    max: '16',
+    avg: '4.6'
+};
+
+
+
