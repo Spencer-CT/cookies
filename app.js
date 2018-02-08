d4c9b428a9f1f2144d900ff8a9760840255c0e65
Spencer Thode <spencerthode@gmail.com>
2018-02-07 16:33:36 -0800

    have base started

--- app.js
+++ app.js
@@ -6,186 +6,205 @@ const pdx = {
     max: 65,
     avg: 6.3,
     timeArray:[],
+        random: function () {
+            let min = Math.ceil(this.min);
+            let max = Math.floor(this.max);
+            for (let i = 0; i < 15; i++){
+            const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
+            const cookPH = Math.round(custPH * this.avg);
+            this.timeArray.push (cookPH);
+            }
+        },
+        totalCookies: function () {
+            let total = 0;
+            for (let j = 0; j < 15; j++){
+            total = total + this.timeArray[j];
+            }
+        this.timeArray.push(total);
+        },
+        makeCookies: function () {
+            for (let i = 0; i < this.timeArray.length; i++ ) {
+                const list = document.getElementById('pdx');
+                const li = document.createElement('li');
+                li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
+                list.appendChild(li);
+                console.log(li);
+            }
+        },
+};
+
+function makeCookies (pdx, category) {
+    this.location = location;
+    this.category = category;
+    this.parentId = category + '-cookies';
+
+    this.answer = null;
+}
+
+question.prototype.ask = function() {
+    this.answer = prompt(this.text);
+    this.render();
+};
+
+question.prototype.render = function (){
+    const ele = document.getElementById(this.parentId)
+    const p = document.createElement("p");
+    p.textContent = `Q: ${this.text} && A: ${this.answer} `;
+    ele.appendChild(p);
+};
+const questionOne = new Question('what time is it?', 'yes');
+questionOne.ask();
+questionOne.render();
+
+const questionTwo = new Question('Ice cream?', 'other');
+questionTwo.ask();
+questionTwo.render();
+
+
+// const pSquare = {
+//     location: 'pSquare',
+//     min: 3,
+//     max: 24,
+//     avg: 1.2,
+//     timeArray:[],
     
-    random: function () {
-        let min = Math.ceil(this.min);
-        let max = Math.floor(this.max);
-        for (let i = 0; i < 15; i++){
-        const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
-        const cookPH = Math.round(custPH * this.avg);
-        this.timeArray.push (cookPH);
-        }
-    },
-    totalCookies: function () {
-        let total = 0;
-        for (let j = 0; j < 15; j++){
-        total = total + this.timeArray[j];
-        }
-    this.timeArray.push(total);
-    },
-    makeCookies: function () {
-        for (let i = 0; i < this.timeArray.length; i++ ) {
-            const list = document.getElementById('pdx');
-            const li = document.createElement('li');
-            li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
-            list.appendChild(li);
-            console.log(li);
+//     random: function () {
+//         let min = Math.ceil(this.min);
+//         let max = Math.floor(this.max);
+//         for (let i = 0; i < 15; i++){
+//         const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
+//         const cookPH = Math.round(custPH * this.avg);
+//         this.timeArray.push (cookPH);
+//         }
+//     },
+//     totalCookies: function () {
+//         let total = 0;
+//         for (let j = 0; j < 15; j++){
+//         total = total + this.timeArray[j];
+//         }
+//     this.timeArray.push(total);
+//     },
+//     makeCookies: function () {
+//         for (let i = 0; i < this.timeArray.length; i++ ) {
+//             const list = document.getElementById('pSquare');
+//             const li = document.createElement('li');
+//             li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
+//             list.appendChild(li);
+//             console.log(li);
+//         }
+//     },
+// };
+
+// pSquare.random();
+// pSquare.totalCookies();
+// pSquare.makeCookies();
+
+// const powells = {
+//     location: 'powells',
+//     min: 11,
+//     max: 38,
+//     avg: 1.7,
+//     timeArray:[],
+    
+//     random: function () {
+//         let min = Math.ceil(this.min);
+//         let max = Math.floor(this.max);
+//         for (let i = 0; i < 15; i++){
+//         const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
+//         const cookPH = Math.round(custPH * this.avg);
+//         this.timeArray.push (cookPH);
+//         }
+//     },
+//     totalCookies: function () {
+//         let total = 0;
+//         for (let j = 0; j < 15; j++){
+//         total = total + this.timeArray[j];
+//         }
+//     this.timeArray.push(total);
+//     },
+//     makeCookies: function () {
+//         for (let i = 0; i < this.timeArray.length; i++ ) {
+//             const list = document.getElementById('powells');
+//             const li = document.createElement('li');
+//             li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
+//             list.appendChild(li);
+//             console.log(li);
+//         }
+//     },
+// };
+
+// powells.random();
+// powells.totalCookies();
+// powells.makeCookies();
+
+// const stJohns = {
+//     location: 'stJohns',
+//     min: 20,
+//     max: 38,
+//     avg: 2.3,
+//     timeArray:[],
+    
+//     random: function () {
+//         let min = Math.ceil(this.min);
+//         let max = Math.floor(this.max);
+//         for (let i = 0; i < 15; i++){
+//         const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
+//         const cookPH = Math.round(custPH * this.avg);
+//         this.timeArray.push (cookPH);
+//         }
+//     },
+//     totalCookies: function () {
+//         let total = 0;
+//         for (let j = 0; j < 15; j++){
+//         total = total + this.timeArray[j];
+//         }
+//     this.timeArray.push(total);
+//     },
+//     makeCookies: function () {
+//         for (let i = 0; i < this.timeArray.length; i++ ) {
+//             const list = document.getElementById('stJohns');
+//             const li = document.createElement('li');
+//             li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
+//             list.appendChild(li);
+//             console.log(li);
+//         }
+//     },
+// };
+
+// stJohns.random();
+// stJohns.totalCookies();
+// stJohns.makeCookies();
+
+// const waterfront = {
+//     location: 'waterfront',
+//     min: 2,
+//     max: 16,
+//     avg: 4.6,
+//     timeArray:[],
+    
+//     random: function () {
+//         let min = Math.ceil(this.min);
+//         let max = Math.floor(this.max);
+//         for (let i = 0; i < 15; i++){
+//         const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
+//         const cookPH = Math.round(custPH * this.avg);
+//         this.timeArray.push (cookPH);
+//         }
+//     },
+//     totalCookies: function () {
+//         let total = 0;
+//         for (let j = 0; j < 15; j++){
+//         total = total + this.timeArray[j];
+//         }
+//     this.timeArray.push(total);
+//     },
+//     makeCookies: function () {
+//         for (let i = 0; i < this.timeArray.length; i++ ) {
+//             const list = document.getElementById('waterfront');
+//             const li = document.createElement('li');
+//             li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
+//             list.appendChild(li);
+//             console.log(li);
         }
     },
 };
-
-pdx.random();
-pdx.totalCookies();
-pdx.makeCookies();
-
-const pSquare = {
-    location: 'pSquare',
-    min: 3,
-    max: 24,
-    avg: 1.2,
-    timeArray:[],
-    
-    random: function () {
-        let min = Math.ceil(this.min);
-        let max = Math.floor(this.max);
-        for (let i = 0; i < 15; i++){
-        const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
-        const cookPH = Math.round(custPH * this.avg);
-        this.timeArray.push (cookPH);
-        }
-    },
-    totalCookies: function () {
-        let total = 0;
-        for (let j = 0; j < 15; j++){
-        total = total + this.timeArray[j];
-        }
-    this.timeArray.push(total);
-    },
-    makeCookies: function () {
-        for (let i = 0; i < this.timeArray.length; i++ ) {
-            const list = document.getElementById('pSquare');
-            const li = document.createElement('li');
-            li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
-            list.appendChild(li);
-            console.log(li);
-        }
-    },
-};
-
-pSquare.random();
-pSquare.totalCookies();
-pSquare.makeCookies();
-
-const powells = {
-    location: 'powells',
-    min: 11,
-    max: 38,
-    avg: 1.7,
-    timeArray:[],
-    
-    random: function () {
-        let min = Math.ceil(this.min);
-        let max = Math.floor(this.max);
-        for (let i = 0; i < 15; i++){
-        const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
-        const cookPH = Math.round(custPH * this.avg);
-        this.timeArray.push (cookPH);
-        }
-    },
-    totalCookies: function () {
-        let total = 0;
-        for (let j = 0; j < 15; j++){
-        total = total + this.timeArray[j];
-        }
-    this.timeArray.push(total);
-    },
-    makeCookies: function () {
-        for (let i = 0; i < this.timeArray.length; i++ ) {
-            const list = document.getElementById('powells');
-            const li = document.createElement('li');
-            li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
-            list.appendChild(li);
-            console.log(li);
-        }
-    },
-};
-
-powells.random();
-powells.totalCookies();
-powells.makeCookies();
-
-const stJohns = {
-    location: 'stJohns',
-    min: 20,
-    max: 38,
-    avg: 2.3,
-    timeArray:[],
-    
-    random: function () {
-        let min = Math.ceil(this.min);
-        let max = Math.floor(this.max);
-        for (let i = 0; i < 15; i++){
-        const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
-        const cookPH = Math.round(custPH * this.avg);
-        this.timeArray.push (cookPH);
-        }
-    },
-    totalCookies: function () {
-        let total = 0;
-        for (let j = 0; j < 15; j++){
-        total = total + this.timeArray[j];
-        }
-    this.timeArray.push(total);
-    },
-    makeCookies: function () {
-        for (let i = 0; i < this.timeArray.length; i++ ) {
-            const list = document.getElementById('stJohns');
-            const li = document.createElement('li');
-            li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
-            list.appendChild(li);
-            console.log(li);
-        }
-    },
-};
-
-stJohns.random();
-stJohns.totalCookies();
-stJohns.makeCookies();
-
-const waterfront = {
-    location: 'waterfront',
-    min: 2,
-    max: 16,
-    avg: 4.6,
-    timeArray:[],
-    
-    random: function () {
-        let min = Math.ceil(this.min);
-        let max = Math.floor(this.max);
-        for (let i = 0; i < 15; i++){
-        const custPH = Math.floor(Math.random() * (max - min + 1)) + min;
-        const cookPH = Math.round(custPH * this.avg);
-        this.timeArray.push (cookPH);
-        }
-    },
-    totalCookies: function () {
-        let total = 0;
-        for (let j = 0; j < 15; j++){
-        total = total + this.timeArray[j];
-        }
-    this.timeArray.push(total);
-    },
-    makeCookies: function () {
-        for (let i = 0; i < this.timeArray.length; i++ ) {
-            const list = document.getElementById('waterfront');
-            const li = document.createElement('li');
-            li.textContent = hr[i] + this.timeArray[i] + ' cookies.';
-            list.appendChild(li);
-            console.log(li);
-        }
-    },
-};
-
-waterfront.random();
-waterfront.totalCookies();
-waterfront.makeCookies();
